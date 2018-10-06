d464abcbb9a920a83bca649a5f3d6b9823cfc270
DenisYurkov <denisdeveloper136@gmail.com>
2018-10-06 15:57:56 +0300

    Commit

--- JavaScript Practice/How to work with API Google/js/script.js
+++ JavaScript Practice/How to work with API Google/js/script.js
@@ -1,6 +1,6 @@
     function initMap() {
        map = new google.maps.Map(document.getElementById('map'), {
-         center: {lat: -34.397, lng: 150.644},
+         center: {lat: -31.397, lng: 150.644},
          zoom: 8
        });
       }
