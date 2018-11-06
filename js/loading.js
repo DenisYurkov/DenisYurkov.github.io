ddb3950a38c7b1021f0b1692bd27c16bb1f2beda
DenisYurkov <denisdeveloper136@gmail.com>
2018-11-06 19:58:09 +0300

    Home Page

---
+++ js/loading.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', function() {
+        QueryLoader2(document.querySelector("body"), {
+            barColor: "#efefef",
+            backgroundColor: "#111",
+            percentage: true,
+            barHeight: 1,
+            minimumTime: 200,
+            fadeOutTime: 1000
+        });
+    });
